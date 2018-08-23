02c4e570e14c80d06f4dc62898883c0f9ae09a26
adrian barbosa <abarbosa20033@columbushs.com>
2018-08-23 13:26:28 -0400

    initial commit

---
+++ src/schedule/Schedule.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedule;
+
+/**
+ *
+ * @author 20033
+ */
+public class Schedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String firstName = "Adrian";
+        String lastName = "barbosa";
+        int gradYear= 2020;
+            System.out.println("My name is " + firstName + " " + lastName +" and I'll graduate in " + gradYear  );
+                
+           
+            
+            
+            
+            
+            
+            
+            int periodOne = 1;
+            int periodTwo = 2;
+            int periodThree = 3;
+            int periodFour = 4;
+            int periodFive = 5;
+            int periodSix = 6;
+            int periodSeven = 7;
+            
+            System.out.println("-------------------------------------------------");
+            String classOne = " English III accelarated         | Mr. Ulloa ";
+            String classTwo = " AP computer science             | Mr. Larrubia ";
+            String classThree = "Honors Global studies            | Mr. Dugard";
+            String classFour = "college Algebra and trigonometry | Mr. Foyo";
+            String classFive = " Environmental science           | Mr.Connell ";
+            String classSix = " American History                | Mr. Benitez";
+            String classSeven = " AP studio design                | Mr. Isenberg";
+            
+            
+            
+            
+            
+        System.out.println("|" + periodOne + "|" + classOne);
+        System.out.println("|" + periodTwo + "|" + classTwo);
+        System.out.println("|" + periodThree + "|" + classThree);
+        System.out.println("|" + periodFour + "|" + classFour);
+        System.out.println("|" + periodFive + "|" + classFive);
+        System.out.println("|" + periodSix + "|" + classSix);
+        System.out.println("|" + periodSeven + "|" + classSeven);
+        
+        System.out.println("-------------------------------------------------");
+    }
+    
+}
